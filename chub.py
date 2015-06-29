601545e74c12a4b9c3f633e70f8b91c99d9baf47
Charles R. Harwood <charles.harwood@zuerchertech.com>
2015-06-29 17:17:04 -0500

    Added basic curses program

---
+++ chub.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env leds_python
+import sys
+import argparse
+import curses
+
+DESCRIPTION = "Chat on GitHub - AKA CHUB"
+
+DEFAULT_HOST = 'https://github.com/Combatjuan/chub'
+VERSION = 0.1
+
+#===============================================================================
+def run(command, cwd=None):
+	"""
+	Runs a command an returns the result code, stdout and stderr.
+	The command is a string parseable as a shell command.
+
+	The cwd option (default current directory) causes the command to be run as though from that directory.
+	"""
+	c = shlex.split(command)
+	p = subprocess.Popen(c, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	stdout, stderr = p.communicate()
+	return (p.returncode, stdout, stderr)
+
+#===============================================================================
+def print_usage(option, opt, value, parser):
+	parser.print_help()
+	sys.exit(0)
+
+# ==============================================================================
+def parse_args():
+	parser = argparse.ArgumentParser(description=DESCRIPTION, add_help=False)
+
+	parser.add_argument("-?", "--help", action="help")
+
+	parser.add_argument('host', action='store', nargs=1, default=DEFAULT_HOST,
+			help='Chat host to connect to.')
+
+	args = parser.parse_args()
+
+	return args
+
+# ==============================================================================
+def begin_chat(host):
+	screen = curses.initscr()
+	curses.start_color()
+	height, width = screen.getmaxyx()
+	vcenter = int(height / 2)
+	screen.move(vcenter, 2)
+	screen.addstr("Welcome to Chub Version {}".format(VERSION))
+	screen.move(vcenter + 1, 2)
+	screen.addstr("'quit' to quit.")
+	return screen
+
+# ==============================================================================
+def chat(host, app):
+	input = ''
+	height, width = app.getmaxyx()
+	app.move(height - 2, 2)
+	app.addstr(">")
+	while input != 'quit':
+		input = app.getstr(height - 2, 4)
+
+# ==============================================================================
+def end_chat(host):
+	curses.endwin()
+
+# ==============================================================================
+def main():
+	args = parse_args()
+	try:
+		app = begin_chat(args.host)
+		chat(args.host, app)
+	# You do not want to accidentally forget to end your curses sessions.  Leaves a bit mess.
+	finally:
+		end_chat(args.host)
+
+# ==============================================================================
+if __name__ == '__main__':
+	main()
+
